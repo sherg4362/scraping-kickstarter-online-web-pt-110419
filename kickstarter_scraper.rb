39fb36f4a6773014e90fd64d749a9a3199152fc4
Sheriefs Gamble <sgamble284@gmail.com>
2020-01-08 15:50:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
 # require libraries/modules here
+require "nokogiri"
+require 'pry'
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
