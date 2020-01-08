d2397bb35f21160fe956f43d0e454985276f6fd0
Sheriefs Gamble <sgamble284@gmail.com>
2020-01-08 16:01:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,13 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
   binding.pry
 end
 
